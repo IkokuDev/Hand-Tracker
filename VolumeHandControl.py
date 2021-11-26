c0b1b0f5ecb4e81cefa485647cfb56d69bc4dd2d
Michael Ikoku <46347708+IkokuDev@users.noreply.github.com>
2021-11-26 10:00:22 +0100

    Add files via upload

---
+++ VolumeHandControl.py
@@ -0,0 +1,68 @@
+import cv2
+import time
+import numpy as np
+import HandTrackingModule as htm
+import math
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+#######################
+wCam, hCam = 640, 480
+#######################
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+
+pTime = 0
+detector = htm.handDetector(detectionCon=int(0.7), maxHands=1)
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(
+    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+#volume.GetMute()
+#volume.GetMasterVolumeLevel()
+volRange = volume.GetVolumeRange()
+
+minVol = volRange[0]
+maxVol = volRange[1]
+volBar = 400
+vol = 0
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+    if len(lmList) != 0:
+
+        x1, y1 = lmList[4][1], lmList[4][2]
+        x2, y2 = lmList[8][1], lmList[8][2]
+        cx, cy = (x1 + x2)//2, (y1 + y2)//2
+
+        cv2.circle(img, (x1, y1), 11, (0, 81, 242), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 11, (0, 81, 242), cv2.FILLED)
+        cv2.circle(img, (cx, cy), 11, (99, 99, 99), cv2.FILLED)
+        cv2.line(img, (x1, y1), (x2, y2), (99, 99, 99), 2)
+
+        length = math.hypot(x2 - x1, y2 - y1)
+
+        vol = np.interp(length, [50, 175], [minVol, maxVol])
+        volBar = np.interp(length, [50, 175], [400, 150])
+
+        volume.SetMasterVolumeLevel(vol, None)
+
+        if length < 50:
+            cv2.circle(img, (cx, cy), 11, (0, 0, 255), cv2.FILLED)
+    cv2.rectangle(img, (50, 150), (54, 400), (255, 255, 255), 1)
+    cv2.rectangle(img, (50, int(volBar)), (54, 400), (255, 255, 255), cv2.FILLED)
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255), 2 )
+
+    cv2.imshow("Volume Control v0.01  Infovision Technology", img)
+    key = cv2.waitKey(1)
+    if key == 81 or key == 113:
+        break
